31817c5d773a3a7c2fa38189f347eb6af368e89b
silvi <silviston@gmail.com>
2026-02-12 15:20:47 +0530

    Upload 2

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 from typing import Dict
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ================= RATE LIMIT CONFIG =================
 RATE_PER_MINUTE = 32
